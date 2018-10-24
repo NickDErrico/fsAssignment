746383839271feeed8f23e8a2445f0170fdfea39
Nick D'Errico <nickderrico@Nicks-MacBook-Pro.local>
2018-10-24 14:01:57 -0700

    solution from lecture video

--- app.js
+++ app.js
@@ -1,60 +1,92 @@
- var fs = require('fs');
+const fs = require("fs");
 
- //challenge 1
- // fs.readFile('./challenge1/info.txt', 'utf-8', (err, data) => {
- //   if (err) {
- //     throw err;
- //   }
- //   console.log(data);
- // })
+//challenge 1
 
- //challenge 2
- // let names = fs.readFileSync('./challenge2/info.txt', 'utf-8')
- // names = names + ' Carlos, Jennifer, Nick, Nick & Troy '
- // fs.writeFileSync('./challenge2/info.txt', names)
+// fs.readFile("./challenge1/info.txt", "utf8", (data, err) => {
+//   if (err) {
+//     console.log(err);
+//   } else {
+//     console.log(data);
+//   }
+// });
+//
+// let file = fs.readFileSync("./challenge1/info.txt", "utf8");
+//
+// console.log(file);
 
- //challenge 3
- // fs.rename('./challenge3/binfo.txt', './challenge3/info.txt', function(err) {
- //   if (err) {
- //     console.log(err);
- //   }
- // })
+//challenge 2
 
- //challenge 4
- // fs.mkdirSync('./challenge4/copyfolder')
- // fs.copyFile('./challenge4/info.txt', './challenge4/copyfolder/info.txt', function(err) {
- //   if (err) {
- //     throw err;
- //   }
- // })
+// let names = ["Nick", "Marcus", "Dennis", "Greg"];
+//
+// fs.appendFile("./challenge2/info.txt", names, (data, err) => {
+//   if (err) {
+//     console.log(err);
+//   } else {
+//     console.log(data);
+//   }
+// });
+//
+// fs.appendFileSync("./challenge2/info.txt", names);
 
- //challenge 5
+//challenge 3
 
- // function updateFile() {
- //   let origFile = fs.readFileSync('./challenge5/info.txt', 'utf-8')
- //   let newFile = " ";
- //   for (let i = 0; i < origFile.length; i++) {
- //     if (origFile[i] === '-') {
- //       newFile += " "
- //     } else {
- //       newFile += origFile[i]
- //     }
- //   }
- //   fs.writeFileSync('./challenge5/info.txt', newFile)
- // }
- //
- // updateFile()
+// fs.rename("./challenge3/binfo.txt", "./challenge3/info.txt", (data, err) => {
+//   if (err) {
+//     console.log(err);
+//   } else {
+//     console.log(data);
+//   }
+// });
+//
+// fs.renameSync("./challenge3/binfo.txt", "./challenge3/info.txt");
 
- //challenge 6
- function findTxtFiles() {
-   let allTxtFiles;
-   let allFiles = fs.readdirSync('./')
-   for (let i = 0; i < allFiles.length; i++) {
-     console.log(allFiles[i]);
-     let folderFiles = fs.readdirSync('./', allFiles[i])
-     console.log(folderFiles);
-   }
- }
- }
+//challenge 4
 
- findTxtFiles()
+// fs.mkdir("./challenge4/copyfolder", (data, err) => {
+//   if (err) {
+//     console.log(err);
+//   } else {
+//     fs.copyFile(
+//       "./challenge4/info.txt",
+//       "./challenge4/copyfolder/myCopy.txt",
+//       (data, err) => {
+//         if (err) {
+//           console.log(err);
+//         } else {
+//           console.log(data);
+//         }
+//       }
+//     );
+//   }
+// });
+
+//challenge 5
+
+let data = fs.readFileSync("./challenge5/info.txt", "utf8");
+let newData = "";
+for (let i = 0; i < data.length; i++) {
+  if (data[i] === "-") {
+    newData += " ";
+  } else {
+    newData += data[i];
+  }
+}
+
+fs.writeFileSync("./challenge5/info.txt", newData);
+
+//challenge 6
+
+let files;
+fs.readdir("./challenge6/", (err, data) => {
+  if (err) {
+    console.log(err);
+  } else {
+    let txtFiles = [];
+    for (let i = 0; i < data.length; i++) {
+      if (data[i].substring(data[i].length - 4) === ".txt") {
+        txtFiles.push(data[i]);
+      }
+    }
+    console.log(txtFiles);
+  }
+});
